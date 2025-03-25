8dfc6d5dab353f8a0dd7fb238df588a2191b5e18
Alexandre Rodrigues <85518165+Alexandre2107@users.noreply.github.com>
2025-03-25 17:50:00 -0400

    Feat: Added number 4 implementations for lab 5

--- react-from-scratch/my-app/src/MyApp.jsx
+++ react-from-scratch/my-app/src/MyApp.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router';
 
 function MyApp() {
     return (
-        <div style={{ textAlign: 'center'}}>
+        <div style={{ textAlign: 'center' }}>
             <h1>Welcome to the Simple E-Commerce App</h1>
             <Link to="/book">Go to Book Management</Link>
-            <Link style={{marginLeft: 20 }} to={"/magazine"}>Go to Magazine Management</Link>
-            <Link style={{marginLeft: 20 }} to={"/discMag"}>Go to DiscMag Management</Link>
-            <Link style={{marginLeft: 20 }} to={"/ticket"}>Go to Ticket Management</Link>
+            <Link style={{ marginLeft: 20 }} to="/magazine">Go to Magazine Management</Link>
+            <Link style={{ marginLeft: 20 }} to="/discMag">Go to DiscMag Management</Link>
+            <Link style={{ marginLeft: 20 }} to="/ticket">Go to Ticket Management</Link>
         </div>
     );
 }
